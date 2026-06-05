67308df59f127234157b1a2c2395967a4aeff311
johnbondu <johnbondhi14@gmail.com>
2026-06-05 14:50:27 +0530

    Solved LeetCode 7 - Reverse Integer

---
+++ 01-Basic-Maths/reverse_integer.py
@@ -0,0 +1,23 @@
+# LeetCode 7 - Reverse Integer
+# Difficulty: Medium
+# Problem Link: https://leetcode.com/problems/reverse-integer/
+
+class Solution:
+
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        rev = 0
+
+        while x:
+            digit = x % 10
+            rev = rev * 10 + digit
+            x //= 10
+
+        rev *= sign
+
+        if rev < -(2**31) or rev > 2**31 - 1:
+            return 0
+
+        return rev
