11b0845ea2b542fd465a975eb3c0476a1c9b2115
johnbondu <johnbondhi14@gmail.com>
2026-06-06 17:08:00 +0530

    Add diamond pattern program in pattern9.py
    Implement diamond pattern printing using asterisks.

---
+++ Patterns/pattern9.py
@@ -0,0 +1,37 @@
+#The pattern is diamond shape using astric 
+problem ----> https://takeuforward.org/plus/dsa/problems/pattern-9?source=strivers-a2z-dsa-track
+
+
+Enter number of lines :5
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * * 
+      * * * 
+        * 
+
+
+n = int(input ("Enter number of lines :"))
+for i in range(1,n+1):
+    #for space
+    for j in range(n-i):
+        print(" ",end=" ")
+    #for astric
+    for k in range(2*i-1):
+        print("*",end =" ")
+    #for new line
+    print()
+
+for i in range(n,0,-1):
+    #for space 
+    for j in range(n-i):
+        print(" ",end=" ")
+    #for pyramid
+    for k in range(2*i-1):
+        print("*",end=" ")
+    print( )
+
