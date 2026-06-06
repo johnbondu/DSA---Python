66813d6219a9e54af66a20ca06bc53de495e3a96
johnbondu <johnbondhi14@gmail.com>
2026-06-06 06:53:57 +0530

    Solved GCD of Two Numbers using Euclid's Algorithm

---
+++ 01-Basic-Maths/gcd_of_two_numbers.py
@@ -0,0 +1,20 @@
+# GCD of Two Numbers
+# Time Complexity: O(log(min(a, b)))
+# Space Complexity: O(1)
+
+class Solution:
+    def gcd(self, a, b):
+
+        while b:
+            a, b = b, a % b
+
+        return a              # This are Just for 2 numbers 
+
+# this is for array 
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        minimum = min(nums)
+        maximum = max(nums)
+        while maximum :
+            minimum , maximum = maximum , minimum % maximum 
+        return minimum
