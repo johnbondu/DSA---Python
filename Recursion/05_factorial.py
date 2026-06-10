b6384cff0675219ddba71986fd824c2fb8df4ca7
john <johnbondhi14@gmail.com>
2026-06-10 15:15:08 +0530

    Add factorial function implementation

---
+++ Recursion/05_factorial.py
@@ -0,0 +1,26 @@
+ returning factorial  of a number ---->
+
+
+required out put like -->
+
+  Enter a number : 5
+120
+
+
+factorial(5)
+= 5 * factorial(4)
+= 5 * 4 * factorial(3)
+= 5 * 4 * 3 * factorial(2)
+= 5 * 4 * 3 * 2 * factorial(1)
+= 5 * 4 * 3 * 2 * 1
+= 120
+
+
+
+def factorial(n):
+    if n==0 or n == 1:
+        return 1
+    return n * factorial(n-1) 
+n = int(input(f"Enter a number : "))
+
+print(factorial(n))
