e1cf31d89881aa78dc1e4ccd97ea8f0184e52201
john <johnbondhi14@gmail.com>
2026-06-12 14:16:05 +0530

    Add Odd or Even checker using bit manipulation
    Implement a function to determine if a number is odd or even using bit manipulation.

---
+++ 01-Basic-Maths/Odd_or_even.py
@@ -0,0 +1,25 @@
+finding even number or  odd number 
+
+
+here iming using Bit manpulation to find that the given number is odd or even
+
+n = int(input("enter a n umnber"))
+if n & 1 : 
+  print("Odd Numbers")
+else:
+  print("Even Numbers")
+
+
+Output ---> 
+#Odd numbers 
+
+  
+  
+  
+  
+  # Here this function converts the values into the  binary vallues than checks both numbers 
+  
+                          #If it return one it will means  odd number otherwise even number
+
+
+  
