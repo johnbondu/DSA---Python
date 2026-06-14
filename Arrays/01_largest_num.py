e44294028baaa39351c69fb8163a647d3faeeec9
john <johnbondhi14@gmail.com>
2026-06-14 22:20:44 +0530

    Add largest number finder in an array
    Implemented a function to find the largest element in an array and provided an example usage.

---
+++ Arrays/01_largest_num.py
@@ -0,0 +1,42 @@
+# Largest Element in an Array
+
+Today I solved a basic array problem: finding the largest element in an array.
+
+## Problem
+
+Given an array of integers, find the largest element present in the array.
+
+### Example
+
+Input:
+10 5 25 8 15
+
+Output:
+25
+
+## My Approach
+
+- Start by assuming the first element is the largest.
+- Traverse through the array one by one.
+- If a larger element is found, update the largest value.
+- After checking all elements, return the largest element.
+
+## Python Code
+
+```python
+nums = list(map(int, input("Enter an array: ").split()))
+
+def find_largest_num(nums):
+    largest = nums[0]
+
+    for num in nums:
+        if num > largest:
+            largest = num
+
+    return largest
+
+print(find_largest_num(nums))
+
+
+their is another way to solve this problem by using the ("Max")
+return max(nums)
